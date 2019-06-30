1b39a5ab83e4e0b79ff402cb3f416104f0b4bb7e
Hemang Bhogayata <hemang249@gmail.com>
2019-07-01 00:06:45 +0530

    Added Infinite Land Generation

--- Assets/Scripts/LandScript.cs
+++ Assets/Scripts/LandScript.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class LandScript : MonoBehaviour
 {
+    [Header("Land Generation")]
+    public GameObject Land;
+    public GameObject Camera_Right;
+    public GameObject Camera_Left;
+    public GameObject LandGenerator;
+    public GameObject MidPoint;
+    public GameObject EndPoint;
+    private bool hasGenerated = false;
 
+
+    [Space]
+    [Header("Land Movement")]
     public float moveSpeed = 5f;
-    // Start is called before the first frame update
+   
+   
     void Start()
     {
         
@@ -16,11 +28,36 @@ public class LandScript : MonoBehaviour
     void Update()
     {
         MoveAhead();
+        CheckForLandGeneration();
+        CheckForLandDestruction();
     }
 
     void MoveAhead()
     {
         Vector3 xAxis = new Vector3(-1f,0f,0f);
         transform.position += xAxis * moveSpeed * Time.deltaTime;
+    }
+
+    void CheckForLandGeneration()
+    {
+        if(MidPoint.transform.position.x < Camera_Right.transform.position.x && !hasGenerated)
+        {
+            GenerateLand();
+            hasGenerated = true;
+        }
+    }
+
+    void CheckForLandDestruction()
+    {
+        if(EndPoint.transform.position.x < Camera_Left.transform.position.x)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void GenerateLand()
+    {
+        Instantiate(Land , LandGenerator.transform.position , LandGenerator.transform.rotation);
+
     }
 }
